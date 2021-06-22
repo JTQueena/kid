097800c4a67b01a7c9765746024df0d290492243
JTQueena <jeffipt@gmail.com>
2021-06-22 11:58:56 +0800

    Add try except to avoid program error

--- 2xAddSubtractMultiply.py
+++ 2xAddSubtractMultiply.py
@@ -4,22 +4,34 @@ print("******************************")
 print("\n**    資優生數學練習程式    **")
 print("\n**  僅提供成綪優異學生使用  **")
 print("\n******************************")
-#v2.0
+
 count=0
+
+def keyinxy(x):
+    """ 避免程式錯誤 """
+    try:
+        x = int(x)
+        return x
+    except:
+        print("請重新輸入")
+        return 0
+
 while True: # 連續出題
-    n1=random.randint(1000,9999) #請出題人員設定範圍
-    n2=random.randint(100,999) #請出題人員設定範圍
+    n1=random.randint(10,20) #請出題人員設定範圍
+    n2=random.randint(1,9) #請出題人員設定範圍
     print("總共答對了",count,"題")
     print("")
     print(n1,"x",n2,"=") #請出題人員設定運算方式
-    keyin=int(input("請輸入答案："))
+    x = input("請輸入答案：")
+    x = keyinxy(x)
     ans=n1*n2 #請出題人員設定運算方式
     print("")
 
-    while keyin != ans:
+    while x != ans:
         print("答錯了，請再試一次")
         print(n1,"x",n2,"=") #請出題人員設定運算方式
-        keyin=int(input("請輸入答案："))
+        x = input("請輸入答案：")
+        x = keyinxy(x)        
     else:
         print("答對了，下一題")
         count+=1
--- 2xdivide.py
+++ 2xdivide.py
@@ -4,25 +4,39 @@ print("******************************")
 print("\n**    資優生數學練習程式    **")
 print("\n**  僅提供成綪優異學生使用  **")
 print("\n******************************")
-#v2.0
-count=15
+
+count=0
+
+def keyinxy(x, y):
+    """ 避免程式錯誤 """
+    try:
+        x = int(x)
+        y = int(y)
+        return x, y
+    except:
+        print("請重新輸入")
+        return (0, 0)
+
+
 while True: # 連續出題
-    n1=random.randint(1000,9999) #請出題人員設定範圍
-    n2=random.randint(100,999) #請出題人員設定範圍
+    n1=random.randint(10,99) #請出題人員設定範圍
+    n2=random.randint(1,9) #請出題人員設定範圍
     print("總共答對了",count,"題")
     print("")
     print(n1,"/",n2,"=") #請出題人員設定運算方式
-    keyind1=int(input("請輸入商數："))
-    keyind2=int(input("請輸入餘數："))
+    x = input("請輸入商數：")
+    y = input("請輸入餘數：")
+    (x, y) = keyinxy(x, y)
     ansd1=n1//n2
     ansd2=n1%n2
     print("")
 
-    while (keyind1,keyind2) != (ansd1,ansd2):
+    while (x, y) != (ansd1,ansd2):
         print("答錯了，請再試一次")
         print(n1,"/",n2,"=") #請出題人員設定運算方式
-        keyind1=int(input("請輸入商數："))
-        keyind2=int(input("請輸入餘數："))    
+        x = input("請輸入商數：")
+        y = input("請輸入餘數：")
+        (x, y) = keyinxy(x, y)
     else:
         print("答對了，下一題")
         count+=1
